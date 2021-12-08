848323cb0e424e8faa934301156501e418e9bea9
Banner <yangdianzhao123@163.com>
2021-12-08 20:32:15 +0800

    1个表白网页
    1

---
+++ static/js/app.dd44f265da874e0d2a25.js
@@ -0,0 +1,339 @@
+webpackJsonp([1], {
+	38: function(n, e, t) {
+		t(84);
+		var r = t(23)(t(40), t(96), "data-v-bd26cd6c", null);
+		n.exports = r.exports
+	},
+	40: function(n, e, t) {
+		"use strict";
+		Object.defineProperty(e, "__esModule", {
+			value: !0
+		});
+		var r = t(24),
+			o = t.n(r),
+			i = t(45),
+			a = t.n(i),
+			s = t(44),
+			l = t.n(s),
+			u = t(93),
+			c = t.n(u),
+			d = t(92),
+			f = t.n(d),
+			m = t(81),
+			h = (t.n(m), function() {
+				for (var n = navigator.userAgent, e = ["Android", "iPhone", "SymbianOS",
+						"Windows Phone", "iPad", "iPod"], t = !0, r = 0; r < e.length; r++)
+					if (n.indexOf(e[r]) > 0) {
+						t = !1;
+						break
+					} return t
+			}()),
+			g = function(n, e) {
+				var t = new Date(Date.parse(n.replace(/-/g, "/"))).getTime(),
+					r = new Date(Date.parse(e.replace(/-/g, "/"))).getTime();
+				return Math.abs(t - r) / 864e5
+			};
+		document.title += g((new Date).getFullYear() + "-" + ((new Date).getMonth() + 1) + "-" + (new Date)
+			.getDate(), "2021-09-8") + 1 + "天", e.default = {
+			name: "app",
+			components: {
+				StyleEditor: c.a,
+				ResumeEditor: f.a
+			},
+			data: function() {
+				return {
+					interval: 27,
+					currentStyle: "",
+					enableHtml: !1,
+					fullStyle: [
+						"/*\n* Hi,小韩同志！\n* 我的宝贝！\n* 生日快乐呀！\n* 同时，\n* 今天也是我们在一起的第一100天呦!\n* （因为作业生日推迟，勉强算是一天啦哈哈哈！）\n* 普天同庆！喜气洋洋！\n* 欢天喜地！欢声笑语！\n* 喜上加囍！喜笑颜开！\n* 我没词了..../(ㄒoㄒ)/~~\n* \n* 小韩同志坐好了嘛？\n* 准备开车了！\n\n* 现在这个网页,\n* 就是个什么都没有的白网页,\n* 我给你变点花样吧~\n* 这个网页,\n* 手机电脑都能看,\n* 我猜猜\n* 你现在用的肯定是：\n" +
+						(h ? "* 电脑" : "* 手机") +
+						"\n*/\n\n/* 首先给元素加上点过渡效果 */\n* {\n  -webkit-transition: all .3s;\n  transition: all .3s;\n}\n/* 白色背景太单调了。来点背景 */\nhtml {\n  color: rgb(222,222,222);\n  background: rgb(0,43,54); \n}\n/* 文字太近了 */\n.styleEditor {\n  padding: .5em;\n  border: 1px solid;\n  margin: .5em;\n  overflow: auto;\n  " +
+						(h ? "width: 48%;height: 96%;" : "width: 96%;height: 50%;") +
+						"\n  font-size: 14px;\n  line-height:1.5;\n}\n/* \n* 一年以前，(不知道你还记不记得，嘿！)\n* 你跟我说被别人用程序告白感动到了，\n* 我玩笑着承诺你也给你做一个，\n* 已经超大胆了，哈哈！\n* 终于实现了，我太难了。\n* 我追到了，哈哈哈，耶!~~~\n* 就像条兜兜转转的小溪，\n* 千转百回，终于流向一起。\n* \n* 随着'老阴谋家'计划的顺利推进,\n* 终于在9月8号走进了你的生活。\n* 哈哈，煽情的话不多说了噻，\n* 留着, \n* 以后换个地方说，嘻！\n\n* 你是我晚上睡觉前最想聊天的人，\n* 我爱这种感觉!\n \n* 下面，请党中央检查我这个理工男，\n* 这段时间来的学习成果，\n* 马马虎虎，写的不好。\n\n* 下面有请！小韩同志出场！耶耶耶！\n* 哗哗哗哗哗哗~\n* 鼓掌声做不出来/(ㄒoㄒ)/~~~）\n\n* 我现在做的这个事情，\n* 一般都是一种叫前端工程师的人来做的，\n* emmmm，\n* 怎么理解呢？\n* 就认为是一种程序猿吧~\n\n* 跟网页相关，\n* 用一种叫Javascript\n* 的计算机语言实现。\n这些代码颜色都一样。加点儿高亮区别来 */\n.token.selector{ color: rgb(133,153,0) }\n.token.property{ color: rgb(187,137,0) }\n.token.punctuation{ color: yellow }\n.token.function{ color: rgb(42,161,152) }\n.token.comment{ color: rgb(177,177,177) }\n/* 加个 3D 效果 */\nhtml{\n  -webkit-perspective: 1000px;\n          perspective: 1000px;\n}\n.styleEditor {\n  position: fixed; \n  " +
+						(h ? "left: 0;" : "left:0;right:0;margin:auto;") +
+						"\n  top: 0; \n  -webkit-transition: none; \n  transition: none;   \n  " +
+						(h ? "-webkit-transform: rotateY(10deg) translateZ(-100px) ;transform: rotateY(10deg) translateZ(-100px) ;" :
+							"-webkit-transform: rotateX(-10deg) translateZ(-100px) ;transform: rotateX(-10deg) translateZ(-100px) ;"
+						) + "\n  " + (h ? "" :
+							"-webkit-transform-origin: 50% 0% 0;transform-origin: 50% 0% 0;") +
+						"\n}\n\n/* 再来一张信纸 */\n.resumeEditor{\n  position: fixed; \n  " + (h ?
+							"right: 0;" : "left:0;right:0;margin:auto;") + "\n  " + (h ?
+							"top: 0;" : "bottom:2%;") + "\n  padding: .5em;  \n  " + (h ?
+							"margin: .5em;" : "") + "\n  " + (h ? "width: 48%;height: 96%;" :
+							"width: 96%;height: 50%;") +
+						"\n  border: 1px solid;\n  color: #222;\n  overflow: auto;\n  font-size: 14px;\n  line-height:1.5;\n  " +
+						(h ? "-webkit-transform: rotateY(-10deg) translateZ(-100px) ;transform: rotateY(-10deg) translateZ(-100px) ;" :
+							"-webkit-transform: rotateX(10deg) translateZ(-100px) ;transform: rotateX(10deg) translateZ(-100px) ;"
+						) + "\n    " + (h ? "" :
+							"-webkit-transform-origin: 50% 0% 0;transform-origin: 50% 0% 0;") +
+						"\n  }\n/* 我开始写了 */\n\n\n",
+						"\n/* 是不是看着有点简陋？\n * 因为这是 Markdown 格式的\n * 一种程序员用来写文档日志的简易语言\n * 翻译成 网页 就行了\n */\n",
+						'\n/* 再加点样式 */\n.resumeEditor{\n  padding: 2em;\n  line-height:1.8;\n}\n.resumeEditor h2{\n  display: inline-block;\n  border-bottom: 1px solid;\n  margin: 1em 0 .5em;\n  font-size:18px;\n}\n.resumeEditor ul,.resumeEditor ol{\n  list-style: none;\n}\n.resumeEditor ul> li::before{\n  content: \'•\';\n  margin-right: .5em;\n}\n.resumeEditor ol {\n  counter-reset: section;\n}\n.resumeEditor ol li::before {\n  counter-increment: section;            \n  content: counters(section, ".") " ";  \n  margin-right: .5em;\n}\n.resumeEditor blockquote {\n  margin: 1em;\n  padding: .5em;\n  background: rgba(221,221,221,.5);\n}\n\n/* OK。完成！ */\n\n'
+					],
+					currentMarkdown: "",
+					fullMarkdown: "小杨老师 & 小韩同志\n----\n\n2020年10月11日。终于，你能来重大了，我比你更激动。\n2021年09月01日。江北机场。最近的初次见面，后座局促不安的你。  \n2021年09月08日凌晨。即阴历八月初二。终是说出心里那些话，我爱上你了，你摊上事了。  \n至今已过去 `" +
+						(g((new Date).getFullYear() + "-" + ((new Date).getMonth() + 1) + "-" + (
+							new Date).getDate(), "2021-09-08") + 1) +
+						"` 天\n距离我喜欢上你，我已数不清有多少天了。\n\n我们的故事\n----\n\n* 磕磕绊绊的保研路\n* 无数次的夜聊谈心\n* 对未来规划的讨论\n* 你生气的样子，我的惴惴不安\n* 你低潮的心情，我笨拙的安慰\n* 一起看的春晚，并随时分享快乐\n* 无数次吐槽 苍白的短消息难以承载情感\n* 网上聊天你的一如往常，我慌乱的心、起伏的情绪和不规律的找你\n* 开学整整一天的“收拾”（不禁感叹女生太强了）\n* 洪崖洞你的侧颜，撩动我的心弦\n* 在B区迷路你泪崩的样子\n* 地铁2号线的风景\n* 一起上的南山，戴头盔拍的照片\n* 夏天秋天和冬天的思群广场观众席、跑道和草坪上\n* 吃腻的沈姐烤鱼\n* 好吃不贵的火锅串串\n* 第一次自驾游的紧张和激动\n* 乌江沿线美如画\n* 仙女山小镇摄影师老板带我们拍的星空剪影\n* 小火车和仙女山大草原\n* 和我们记录的点点滴滴\n* 成都学习一周的想你\n* 你和我生过的气，我和你生过的气\n* 恋爱中我们对自身行为做的改正\n* 一起白嫖过的电影哈哈哈\n* 我们的情侣装\n* 吃饭时你的挑挑拣拣，我的日益肥胖\n* 你高超的王者荣耀技术\n* 和你不能同时放在屏幕上的两个手\n* 网盘里满满你的录像\n* ……\n\n一起看过的电影和剧\n----\n\n1. 一条狗的使命\n2. 亲爱的，不要跨过那条江\n3. 头号玩家\n4. 我和我的父辈\n5. 长津湖\n6. 盛夏未来\n7. 你好李焕英\n8. 扬名立万\n9. 鸡毛飞上天\n10. 闯关东\n11. 越狱（未完待续）\n12. 老友记（未完待续）\n13. ……\n\n> 【Every time you come to mind, I realize I'm smiling.】  \n> 借用余秋雨的一句话：你的过去我来不及参与，你的未来我奉陪到底。\n\n"
+				}
+			},
+			created: function() {
+				this.makeResume()
+			},
+			methods: {
+				makeResume: function() {
+					function n() {
+						return e.apply(this, )
+					}
+					var e = l()(a.a.mark(function n() {
+						return a.a.wrap(function(n) {
+							for (;;) switch (n.prev = n.next) {
+								case 0:
+									return n.next = 2, this
+										.progressivelyShowStyle(0);
+								case 2:
+									return n.next = 4, this
+										.progressivelyShowResume();
+								case 4:
+									return n.next = 6, this
+										.progressivelyShowStyle(1);
+								case 6:
+									return n.next = 8, this.showHtml();
+								case 8:
+									return n.next = 10, this
+										.progressivelyShowStyle(2);
+								case 10:
+								case "end":
+									return n.stop()
+							}
+						}, n, this)
+					}));
+					return n
+				}(),
+				showHtml: function() {
+					var n = this;
+					return new o.a(function(e, t) {
+						n.enableHtml = !0, e()
+					})
+				},
+				progressivelyShowStyle: function(n) {
+					var e = this;
+					return new o.a(function(t, r) {
+						var o = e.interval,
+							i = l()(a.a.mark(function e() {
+								var r, s, l, u, c, d = this;
+								return a.a.wrap(function(e) {
+									for (;;) switch (e.prev = e.next) {
+										case 0:
+											if (r = this.fullStyle[n]) {
+												e.next = 3;
+												break
+											}
+											return e.abrupt("return");
+										case 3:
+											s = this.fullStyle.filter(
+													function(e, t) {
+														return t <= n
+													}).map(function(n) {
+													return n.length
+												}).reduce(function(n,
+													e) {
+													return n + e
+												}, 0), l = s - r.length,
+												this.currentStyle
+												.length < s ? (u = this
+													.currentStyle
+													.length - l, c = r
+													.substring(u, u +
+														1) || " ", this
+													.currentStyle += c,
+													"\n" === r
+													.substring(u - 1,
+														u) && this.$refs
+													.styleEditor && this
+													.$nextTick(
+														function() {
+															d.$refs
+																.styleEditor
+																.goBottom()
+														}), setTimeout(
+														i, o)
+												) : t();
+										case 6:
+										case "end":
+											return e.stop()
+									}
+								}, e, this)
+							})).bind(e);
+						i()
+					})
+				},
+				progressivelyShowResume: function() {
+					var n = this;
+					return new o.a(function(e, t) {
+						var r = n.fullMarkdown.length,
+							o = n.interval;
+						! function t() {
+							if (n.currentMarkdown.length < r) {
+								n.currentMarkdown = n.fullMarkdown.substring(0, n
+									.currentMarkdown.length + 1);
+								n.currentMarkdown[n.currentMarkdown.length - 1];
+								"\n" === n.currentMarkdown[n.currentMarkdown.length - 2] &&
+									n.$refs.resumeEditor && n.$nextTick(function() {
+										return n.$refs.resumeEditor.goBottom()
+									}), setTimeout(t, o)
+							} else e()
+						}()
+					})
+				}
+			}
+		}
+	},
+	41: function(n, e, t) {
+		"use strict";
+		Object.defineProperty(e, "__esModule", {
+			value: !0
+		});
+		var r = t(85),
+			o = t.n(r);
+		e.default = {
+			props: ["markdown", "enableHtml"],
+			name: "ResumeEditor",
+			computed: {
+				result: function() {
+					return this.enableHtml ? o()(this.markdown) : this.markdown
+				}
+			},
+			methods: {
+				goBottom: function() {
+					this.$refs.container.scrollTop = 1e5
+				}
+			}
+		}
+	},
+	42: function(n, e, t) {
+		"use strict";
+		Object.defineProperty(e, "__esModule", {
+			value: !0
+		});
+		var r = t(86),
+			o = t.n(r);
+		e.default = {
+			name: "Editor",
+			props: ["code"],
+			computed: {
+				highlightedCode: function() {
+					return o.a.highlight(this.code, o.a.languages.css)
+				},
+				codeInStyleTag: function() {
+					return "<style>" + this.code + "</style>"
+				}
+			},
+			methods: {
+				goBottom: function() {
+					this.$refs.container.scrollTop = 1e5
+				}
+			}
+		}
+	},
+	43: function(n, e, t) {
+		"use strict";
+		Object.defineProperty(e, "__esModule", {
+			value: !0
+		});
+		var r = t(39),
+			o = t(38),
+			i = t.n(o);
+		new r.a({
+			el: "#app",
+			render: function(n) {
+				return n(i.a)
+			}
+		})
+	},
+	81: function(n, e) {},
+	82: function(n, e) {},
+	83: function(n, e) {},
+	84: function(n, e) {},
+	92: function(n, e, t) {
+		t(83);
+		var r = t(23)(t(41), t(95), "data-v-3539023c", null);
+		n.exports = r.exports
+	},
+	93: function(n, e, t) {
+		t(82);
+		var r = t(23)(t(42), t(94), "data-v-23b5cc2c", null);
+		n.exports = r.exports
+	},
+	94: function(n, e) {
+		n.exports = {
+			render: function() {
+				var n = this,
+					e = n.$createElement,
+					t = n._self._c || e;
+				return t("div", {
+					ref: "container",
+					staticClass: "styleEditor"
+				}, [t("div", {
+					staticClass: "code",
+					domProps: {
+						innerHTML: n._s(n.codeInStyleTag)
+					}
+				}), n._v(" "), t("pre", {
+					domProps: {
+						innerHTML: n._s(n.highlightedCode)
+					}
+				})])
+			},
+			staticRenderFns: []
+		}
+	},
+	95: function(n, e) {
+		n.exports = {
+			render: function() {
+				var n = this,
+					e = n.$createElement,
+					t = n._self._c || e;
+				return t("div", {
+					ref: "container",
+					staticClass: "resumeEditor",
+					class: {
+						htmlMode: n.enableHtml
+					}
+				}, [n.enableHtml ? t("div", {
+					domProps: {
+						innerHTML: n._s(n.result)
+					}
+				}) : t("pre", [n._v(n._s(n.result))])])
+			},
+			staticRenderFns: []
+		}
+	},
+	96: function(n, e) {
+		n.exports = {
+			render: function() {
+				var n = this,
+					e = n.$createElement,
+					t = n._self._c || e;
+				return t("div", {
+					attrs: {
+						id: "app"
+					}
+				}, [t("StyleEditor", {
+					ref: "styleEditor",
+					attrs: {
+						code: n.currentStyle
+					}
+				}), n._v(" "), t("ResumeEditor", {
+					ref: "resumeEditor",
+					attrs: {
+						markdown: n.currentMarkdown,
+						enableHtml: n.enableHtml
+					}
+				})], 1)
+			},
+			staticRenderFns: []
+		}
+	}
+}, [43]);
